34f1b24a1eada17ff2b75000032d5c84346f8614
GreenBob94 <86076121+GreenBob94@users.noreply.github.com>
2023-04-04 18:57:06 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    temp = "" + user_key + "A"
+    check_key()
+})
+function check_key () {
+    user_key = temp
+    serial.writeLine(user_key)
+    if (user_key == key) {
+        basic.showNumber(secret)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    temp = "" + user_key + "B"
+    check_key()
+})
+let temp = ""
+let user_key = ""
+let key = ""
+let secret = 0
+secret = 3933
+key = "ABAABB"
+user_key = ""
